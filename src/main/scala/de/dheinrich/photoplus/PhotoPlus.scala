76637551a06360074d0e50900814fae85d8cde54
Daniel Heinrich <dannynullzwo@gmail.com>
2013-10-28 02:38:13 +0100

    dunno

--- src/main/scala/de/dheinrich/photoplus/PhotoPlus.scala
+++ src/main/scala/de/dheinrich/photoplus/PhotoPlus.scala
@@ -1,10 +1,12 @@
 package de.dheinrich.photoplus
 
+import scala.util.control.Exception._
 import com.google.gdata.data.PlainTextConstruct
 import com.google.gdata.client.photos.PicasawebService
 import com.google.gdata.data.photos._
 import java.io.FileInputStream
-import java.net.URL
+import java.net.URI
+import java.net.URL 
 import java.nio.file.Files
 import java.util.ResourceBundle
 import javafx.scene.input.MouseEvent
@@ -28,7 +30,7 @@ import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout.FlowPane
 import scalafx.util.StringConverter
 import javax.imageio.ImageIO
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import eu.hansolo.fx.validation.ValidationPane
 import eu.hansolo.fx.validation.Validator.State._
 import java.io.File
@@ -52,7 +54,7 @@ case class ImageProgress(url_ : String) {
 }
 
 class PhotoPlusController extends jfxf.Initializable {  
-  
+  var picasaService : PicasawebService = _
   //login pane
   @jfxf.FXML
   var loginPane: jfxs.layout.Pane = _
@@ -67,11 +69,12 @@ class PhotoPlusController extends jfxf.Initializable {
   @jfxf.FXML
   var appPane: jfxs.layout.Pane = _
   @jfxf.FXML
-  var albums: jfxc.ComboBox[String] = _
+  var albums: jfxc.ComboBox[GphotoEntry[_]] = _
   @jfxf.FXML
   var button: jfxc.Button = _
   @jfxf.FXML
   var totalProgress: jfxc.ProgressBar = _
+  
   //tabel
   @jfxf.FXML
   var imageTable: jfxc.TableView[ImageProgress] = _
@@ -90,28 +93,54 @@ class PhotoPlusController extends jfxf.Initializable {
     new TableView(imageTable).items = images
     
     loginValidation.addAll(username, password)
+    
+    val fromString = (s:String) => {
+      if(!s.isEmpty){
+        val e = new GphotoEntry[Nothing]()
+        e.setTitle(new PlainTextConstruct(s))
+        e.setId(null)
+        e
+      }else null
+    }
+    
+    val toString = (e: GphotoEntry[_]) => if(e!=null) e.getTitle.getPlainText else ""
+    
+    val converter = StringConverter[GphotoEntry[_]](fromString, toString);
+    val cellFactory = TextFieldListCell.forListView(converter)
+    albums.cellFactory = cellFactory
+    
+    albums.getSelectionModel.selectedItem onChange ((_, _, selected:Any) => { 
+        selected match {
+          case selectedAlbum:GphotoEntry[_] => 
+            images.clear
+            getThumbnailUrls(selectedAlbum) foreach addToPanel
+        }
+      }) 
   }
 
   @jfxf.FXML
   def onFilesDropped(event: DragEvent) {
     val db = event.getDragboard();
     if (db.hasFiles()) {
-      val files = db.getFiles.dropRight(1)      
+      val files = db.getFiles.asScala.dropRight(1)      
       println(s"loading ${files.size} Images...")
 
-      for (file <- files) {
-        images += ImageProgress(file.getAbsolutePath)
-      }
+      files foreach addToPanel
     }
     event.setDropCompleted(db.hasFiles());
     event.consume();
   }
   
+  def addToPanel(f:File) = {images += ImageProgress(f.getAbsolutePath)}
+  def addToPanel(u:URI) = {images += ImageProgress(u.toString)}
+  
   @jfxf.FXML
   def validate(ev: KeyEvent){
-    println(ev.text)
-    val state = if(ev.text isEmpty) INVALID else VALID    
-    loginValidation.setState(ev.source.asInstanceOf[jfxs.Node], state)
+    ev.source match{
+      case field : jfxc.TextField =>    
+        val state = if(field.getCharacters.length == 0) INVALID else VALID    
+        loginValidation.setState(field, state)
+    }    
   }
 
   @jfxf.FXML
@@ -127,21 +156,28 @@ class PhotoPlusController extends jfxf.Initializable {
   @jfxf.FXML
   def onLogin(){
     println("loging in...")
-    try{
-      val a = getAlbums(username.getText, password.getText)  
-      val b = new ObservableBuffer[String]
-      b.appendAll(a)
-      albums.items = b   
-    
-      loginPane.setVisible(false)    
-      appPane.setDisable(false)
-    }catch{
-      case _:Throwable => {
-          println("uppss")          
-          loginValidation.setState(username, INVALID)
-          loginValidation.setState(password, INVALID)
-        }
-    }
+    val service = createPicasaService(username.getText, password.getText) 
+    service match {
+      case Right(serv) =>    
+        picasaService = serv
+        albums.items = ObservableBuffer(getAlbums())      
+        loginPane.setVisible(false)    
+        appPane.setDisable(false)
+      case _ =>
+        println("uppss")          
+        loginValidation.setState(username, INVALID)
+        loginValidation.setState(password, INVALID)
+    }    
+  }
+
+  @jfxf.FXML
+  def onLogout(){  
+    loginPane.setVisible(true)    
+    appPane.setDisable(true)
+  }
+  
+  @jfxf.FXML
+  def onAlbumSelected(){  
   }
   
   @jfxf.FXML
@@ -156,16 +192,28 @@ class PhotoPlusController extends jfxf.Initializable {
 //    val insertedEntry = myService.insert(postUrl, myAlbum);
   }
   
-  def getAlbums(username:String, password:String) = {
-    val myService = new PicasawebService("exampleCo-exampleApp-1")
+  def createPicasaService(username:String, password:String) = allCatch.either{    
+    val myService = new PicasawebService("dheinrich-PhotoPlus-0.1")
     myService.setUserCredentials(username, password)
-
+    myService
+  }
+  
+  def getAlbums() : Seq[GphotoEntry[_]]= {
     val feedUrl = new URL(PhotoPlus.picassaUrl + "?kind=album&access=all");
 
-    val myUserFeed = myService.getFeed(feedUrl, classOf[UserFeed]);
-
-    val albums:Seq[GphotoEntry[_]] = myUserFeed.getEntries().asInstanceOf[java.util.List[GphotoEntry[_]]]    
-    albums map(_.getTitle.getPlainText)
+    val myUserFeed = picasaService.getFeed(feedUrl, classOf[UserFeed]);    
+    myUserFeed.getEntries().asInstanceOf[java.util.List[GphotoEntry[_]]].asScala
+  }
+  
+  def getThumbnailUrls(album: GphotoEntry[_]) = {    
+    val feedUrl = new URL(PhotoPlus.picassaUrl + s"/albumid/${album.getId}");
+    println(feedUrl)
+    val photoFeed = picasaService.getFeed(feedUrl, classOf[AlbumFeed]);
+    val photos = photoFeed.getPhotoEntries().asScala;
+    for(photo <- photos) yield{
+      val url = photo.getMediaThumbnails.get(0).getUrl
+      new URI(url)
+    }
   }
   
   implicit def toSfxTableColumn[E,S](c:jfxs.control.TableColumn[E,S]) = new TableColumn(c)
